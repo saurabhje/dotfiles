8724b6eedf21e807ef61bc63fb61b5641e3a6e72
saurabhje <work.saurabh@outlook.com>
2024-07-04 16:36:01 +0530

    Add: LSP support

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,28 @@
 return {
-  'nvim-telescope/telescope.nvim', 
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>fs", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>gs", builtin.git_files, {})
-  end
+  {  'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>fs", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>gs", builtin.git_files, {})
+    end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+    require("telescope").setup ({
+      extensions = {
+	["ui-select"] = {
+	  require("telescope.themes").get_dropdown {
+	  }
+	}
+      }
+    })
+    require("telescope").load_extension("ui-select")
+    end
+  },
 }
+
 
