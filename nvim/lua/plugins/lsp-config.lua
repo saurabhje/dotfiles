fa27c7de07c24de10dbee54a0ae47a839cb72339
saurabhje <work.saurabh@outlook.com>
2024-07-04 18:29:08 +0530

    Add: Autocomplete

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -7,18 +7,18 @@ return{
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "tsserver" }
-      })
-    end
+    lazy = false,
+    opts = {
+      auto_install = true,
+    }
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({})
-      lspconfig.lua_ls.setup({})
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      lspconfig.tsserver.setup({ capabilities = capabilities})
+      lspconfig.lua_ls.setup({ capabilities =  capabilities})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({"n", "v"} , "<leader>ca", vim.lsp.buf.code_action, {})
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,6 +1,16 @@
 vim.cmd("set shiftwidth=2")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.o.number = true
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
