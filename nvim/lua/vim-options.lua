cbd3dac3367f70ddd9b39864a86f408623cf21da
saurabhje <work.saurabh@outlook.com>
2024-07-06 15:27:20 +0530

    Number

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -7,6 +7,7 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.o.number = true
+vim.o.relativenumber = true
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader><leader>", ":noh<CR>")
 
