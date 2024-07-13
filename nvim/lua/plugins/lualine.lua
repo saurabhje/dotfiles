951f93218e02da67e853610734043697f85aa9b4
saurabhje <work.saurabh@outlook.com>
2024-07-13 18:30:08 +0530

    Added new colorscheme for editor, lualine, tmuxline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,10 @@
-return{
-    'nvim-lualine/lualine.nvim',
-    config = function()
-      require("lualine").setup({
-      options = {
-	theme = "gruvbox"
-	}
-      })
-    end
+return {
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "nightfly",
+			},
+		})
+	end,
 }
-
